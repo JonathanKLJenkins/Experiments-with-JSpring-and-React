d635128e45ebc56c360b97d2aece0c337eeeee77
Jonathan <Jonathan@DESKTOP-JJB2CA6>
2019-11-30 10:07:15 -0500

    This is an initial commit of a JSpring project.

---
+++ Encounter-Generator/src/main/java/Dnd/Encounter_Generator/App.java
@@ -0,0 +1,13 @@
+package Dnd.Encounter_Generator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
